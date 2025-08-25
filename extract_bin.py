27a655b1c910ae8842ae3b26651d0e50b427e996
pingjuu <pung0552@gmail.com>
2025-08-25 18:51:02 +0900

    add png exe py

--- extract_bin.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-with open("b64encoded_macro.txt") as f:
-    data = f.readlines()[0].encode("UTF-8")
-pt = base64.b64decode(data)
-
-
-with open("SystemFailureReporter.exe", "wb") as f:
-    f.write(pt)
